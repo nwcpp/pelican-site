88df48fb11f89cb34f381a92932549ca6bda4061
George V. Reilly <nwcpp2013@gmail.com>
2013-01-13 01:20:08 -0800

    Updated blogroll, social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,14 +10,26 @@ TIMEZONE = 'America/Los_Angeles'
 DEFAULT_LANG = u'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  ((u'Standard C++ Foundation', 'http://isocpp.org/'),
+          (u'Sutter’s Mill', 'http://herbsutter.com/'),
+          (u'Visual C++ Team Blog', 'http://blogs.msdn.com/b/vcblog/'),
+          (u'C++ Soup!', 'http://www.cplusplus-soup.com/'),
+          (u'Dr. Dobb’s C++', 'http://www.drdobbs.com/cpp'),
+          (u'C++Next', 'http://cpp-next.com/'),
+          (u'Scott Meyers', 'http://scottmeyers.blogspot.com/'),
+          (u'Thinking Asynchronously in C++', 'http://blog.think-async.com/'),
+          (u'Anthony Williams', 'http://www.justsoftwaresolutions.co.uk/cplusplus/'),
+          (u'Danny Kalev', 'http://www.informit.com/authors/bio.aspx?a=e19aded6-574c-4c46-8511-101f9f0ed8f8'),
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ((u'Announcements List', 'http://groups.google.com/group/NwcppAnnounce'),
+          (u'Twitter', 'https://twitter.com/nwcpp'),
+          (u'Facebook', 'https://www.facebook.com/groups/344125680930/'),
+          (u'Google+', 'https://plus.google.com/104974891006782790528/posts'),
+          (u'Vimeo', 'http://vimeo.com/nwcpp'),
+          (u'LinkedIn', 'http://www.linkedin.com/groups?gid=2770106'),
+         )
 
 DEFAULT_PAGINATION = 12
 
