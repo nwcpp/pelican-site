adc6a4ace1bc296416dbf90ff424c27f076c4f08
George V. Reilly <george@reilly.org>
2014-04-19 19:55:59 -0700

    Set RELATIVE_URLS for local and remote

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,3 +43,6 @@ DEFAULT_PAGINATION = 12
 
 STATIC_PATHS = ['talks','images']
 TYPOGRIFY = True
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
