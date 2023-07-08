8f0665f89e5cb555294d9f7532783f6698f491f1
Peter Williamson <petewil00@hotmail.com>
2023-07-08 11:21:21 -0700

    Migrate fabfile.py to Python3, and remove unused rackspace commands.

--- fabfile.py
+++ fabfile.py
@@ -1,101 +1,63 @@
-from fabric.api import *
-import fabric.contrib.project as project
+from fabric import task
 import os
 import shutil
 import sys
-import SimpleHTTPServer
-import SocketServer
+import http.server
+import socketserver
 
 # Local path configuration (can be absolute or relative to fabfile)
-env.deploy_path = 'output'
-DEPLOY_PATH = env.deploy_path
+DEPLOY_PATH = 'output'
 
 # Remote server configuration
 production = '$ssh_user@$ssh_host:$ssh_port'
 dest_path = '$ssh_target_dir'
 
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = '$cloudfiles_username'
-env.cloudfiles_api_key = '$cloudfiles_api_key'
-env.cloudfiles_container = '$cloudfiles_container'
-
 # Github Pages configuration
-env.github_pages_branch = "gh-pages"
+github_pages_branch = "gh-pages"
 
 # Port for `serve`
 PORT = 9000
 
-def clean():
+@task
+def clean(context):
     """Remove generated files"""
     if os.path.isdir(DEPLOY_PATH):
         shutil.rmtree(DEPLOY_PATH)
         os.makedirs(DEPLOY_PATH)
-
-def build():
+@task
+def build(context):
     """Build local version of site"""
-    local('pelican -s pelicanconf.py')
+    context.run('pelican -s pelicanconf.py')
 
-def rebuild():
+@task
+def rebuild(context):
     """`clean` then `build`"""
     clean()
     build()
 
-def regenerate():
-    """Automatically regenerate site upon file modification"""
-    local('pelican -r -s pelicanconf.py')
-
-def serve():
+@task
+def serve(context):
     """Serve site at http://localhost:8000/"""
-    os.chdir(env.deploy_path)
+    os.chdir(DEPLOY_PATH)
 
-    class AddressReuseTCPServer(SocketServer.TCPServer):
+    class AddressReuseTCPServer(socketserver.TCPServer):
         allow_reuse_address = True
 
-    server = AddressReuseTCPServer(('', PORT), SimpleHTTPServer.SimpleHTTPRequestHandler)
+    server = AddressReuseTCPServer(('', PORT), http.server.SimpleHTTPRequestHandler)
 
     sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
     server.serve_forever()
 
-def reserve():
+@task
+def reserve(context):
     """`build`, then `serve`"""
     build()
     serve()
 
-def preview():
-    """Build production version of site"""
-    local('pelican -s publishconf.py')
-
-def cf_upload():
-    """Publish to Rackspace Cloud Files"""
-    rebuild()
-    with lcd(DEPLOY_PATH):
-        local('swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-              '-U {cloudfiles_username} '
-              '-K {cloudfiles_api_key} '
-              'upload -c {cloudfiles_container} .'.format(**env))
-
-@hosts(production)
-def publish():
-    """Publish to production via rsync"""
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True,
-        extra_opts='-c',
-    )
-
-def gh_pages():
+@task
+def gh_pages(context):
     """Publish to GitHub Pages"""
     rebuild()
-    local("ghp-import -b {github_pages_branch} {deploy_path}".format(**env))
-    local("git push origin {github_pages_branch}".format(**env))
-
-def format_readme():
-    """Format README.rst with Docutils."""
-    rst2html = os.path.join(
-            os.getenv("VIRTUAL_ENV"),
-            "Scripts" if sys.platform == 'win32' else "bin",
-            "rst2html.py")
-    local('python "{0}" README.rst README.html'.format(rst2html))
+    context.run("ghp-import -b {github_pages_branch} {deploy_path}"
+          .format(github_pages_branch, DEPLOY_PATH))
+    context.run("git push origin {github_pages_branch}".format(github_pages_branch))
